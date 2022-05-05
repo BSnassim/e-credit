d208129ffc37303e53be3b365fca14ec675b1fa3
nassim <naseem.bensalah@gmail.com>
2022-05-05 15:40:52 +0100

    cleared project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,6 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  public appPages = [
-    { title: 'Accueil', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Mes credits', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Demander un credit', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Logout', url: '/folder/Archived', icon: 'archive' },
-  ];
+  
   constructor() {}
 }
