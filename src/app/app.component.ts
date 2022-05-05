387d071e3d41acc1d60470337feeab2c6edf2f4d
nassim <naseem.bensalah@gmail.com>
2022-05-05 15:34:36 +0100

    new branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+    { title: 'Accueil', url: '/folder/Inbox', icon: 'mail' },
+    { title: 'Mes credits', url: '/folder/Outbox', icon: 'paper-plane' },
+    { title: 'Demander un credit', url: '/folder/Favorites', icon: 'heart' },
+    { title: 'Logout', url: '/folder/Archived', icon: 'archive' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
 }
