3a021e620f674724614a9c7304c8df65ab14545f
AKM-FR <khalil-frikha@outlook.fr>
2022-05-23 17:50:02 +0000

    21st commit

--- src/app/credit/notification/notification.component.ts
+++ src/app/credit/notification/notification.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DemandeRdv } from 'src/app/models/demande-rdv';
-// import { TokenService } from 'src/app/auth/services/token.service';
-// import { AppMainComponent } from 'src/app/main/app-main/app-main.component';
 
-import { EventsService } from 'src/app/Services/events.service';
+import { EventsService } from 'src/app/services/events.service';
 
 @Component({
   selector: 'app-notification',
@@ -15,30 +13,13 @@ export class NotificationComponent implements OnInit {
 
   events: DemandeRdv[] = [];
 
-  constructor(
-    // public appMain: AppMainComponent,
-    // private tokenService: TokenService,
-    private eventsService: EventsService
-  ) {}
+  constructor(private eventsService: EventsService) {}
 
   ngOnInit() {
     this.getEvent(2);
   }
-  // doRefresh(event) {
-  //   setTimeout(() => {
-  //     this.getUser().then((res) => {
-  //       this.getEvent(res.id);
-  //     });
-  //     event.target.complete();
-  //   }, 2000);
-  // }
-  // async getUser() {
-  //   const res = await this.tokenService.getUser().toPromise();
-  //   return res;
-  // }
   getEvent(id: number) {
     this.eventsService.getRdvByIdUserAPI(id).subscribe((data) => {
-      console.log(data);
       this.events = data;
     });
   }
